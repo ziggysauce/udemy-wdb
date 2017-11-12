349740b26ed6dcaaba2dda7f76a3b595931db0f9
Dan Nguyen <dtn3423@gmail.com>
2017-11-12 13:38:16 -0500

    basic js practice

---
+++ Section 8/calc.js
@@ -0,0 +1,5 @@
+var age = prompt ("How old are you in years?");
+
+var days = age * 365.25;
+
+alert(age + " years is roughly " + days + " days.");
---
+++ Section 8/stalker.js
@@ -0,0 +1,6 @@
+var first = prompt("What is your first name?");
+var last = prompt("What is your last name?");
+var age = prompt("What is your age in years?");
+
+console.log("Your name, " + first + " " + last + ", is very familiar.");
+console.log("I didn't know you were " + age + " years old.");
